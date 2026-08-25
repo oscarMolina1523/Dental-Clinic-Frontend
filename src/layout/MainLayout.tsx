34c13ab6dc2d46e12f2c2d1486d46a1ea958c030
oscarMolina1523 <oscarmolina1523@gmail.com>
2026-08-25 12:09:08 -0600

    feat: add topbar component in main layout and dynamic routes name

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -2,14 +2,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import AppSidebar from "../shared/AppSidebar";
+import TopBar from "../shared/TopBar";
 
 const MainLayout: React.FC = () => {
-  return (
-    <div className="w-screen h-screen flex flex-row">
-        <AppSidebar/>
-       <Outlet />
-    </div>
-  );
+    return (
+        <div className="w-screen h-screen flex flex-row">
+            <AppSidebar />
+            <div className="flex flex-col w-full h-full">
+                <TopBar />
+                <Outlet />
+            </div>
+        </div>
+    );
 };
 
 export default MainLayout;
