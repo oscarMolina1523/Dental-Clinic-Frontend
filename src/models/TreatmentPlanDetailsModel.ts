dde554f3bb51dd2aac201b92977c7899631fc369
oscarMolina1523 <oscarmolina1523@gmail.com>
2026-09-12 08:01:19 -0600

    feat: add models of treatment plan and treatment plan details

---
+++ src/models/TreatmentPlanDetailsModel.ts
@@ -0,0 +1,42 @@
+import type { TreatmentPlanDetailStatus } from "../utils/treatmentPlanStatus.enum";
+import BaseModel from "./BaseModel";
+
+export default class TreatmentPlanDetail extends BaseModel {
+  planId: string;
+  treatmentId: string;
+   toothNumber: number;
+   quantity: number;
+   unitPrice: number; //almacena el precio del momento que tenia el tratamiento, asi en el futuro cuando aumente el precio este tendra el de ese momento y no se cambiara
+   subtotal: number;
+  status: TreatmentPlanDetailStatus;
+
+  constructor({
+    id,
+    planId,
+    treatmentId,
+    toothNumber,
+    quantity,
+    unitPrice,
+    subtotal,
+    status,
+  }: {
+    id: string;
+    planId: string;
+    treatmentId: string;
+    toothNumber: number;
+    quantity: number;
+    unitPrice: number;
+    subtotal: number;
+    status: TreatmentPlanDetailStatus;
+  }) {
+    super(id);
+
+    this.planId = planId;
+    this.treatmentId = treatmentId;
+    this.toothNumber = toothNumber;
+    this.quantity = quantity;
+    this.unitPrice = unitPrice;
+    this.subtotal = subtotal;
+    this.status = status;
+  }
+}
---
+++ src/models/TreatmentPlanModel.ts
@@ -0,0 +1,44 @@
+
+import type { TreatmentPlanStatus } from "../utils/treatmentPlanStatus.enum";
+import BaseModel from "./BaseModel";
+
+export default class TreatmentPlan extends BaseModel {
+  patientId: string;
+  dentistId: string;
+  code: string;
+  status: TreatmentPlanStatus;
+  totalAmount: number; // Subtotal de todos los TreatmentPlanDetail
+  discount: number;    // Descuento aplicado al plan
+  createdAt: Date;
+
+  constructor({
+    id,
+    patientId,
+    dentistId,
+    code,
+    status,
+    totalAmount,
+    discount,
+    createdAt,
+  }: {
+    id: string;
+    patientId: string;
+    dentistId: string;
+    code: string;
+    status: TreatmentPlanStatus;
+    totalAmount: number;
+    discount: number;
+    createdAt: Date;
+  }) {
+    super(id);
+
+    this.patientId = patientId;
+    this.dentistId = dentistId;
+    this.code = code;
+    this.status = status;
+    this.totalAmount = totalAmount;
+    this.discount = discount;
+    this.createdAt = createdAt;
+  }
+
+}
